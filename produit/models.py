703550779914e6516ec2b879c6eccb32d8b18965
PINTO_ALVES_AN-ESIG <elv-andre.pntlv@eduge.ch>
2020-12-11 11:56:48 +0100

    migration

--- produit/models.py
+++ produit/models.py
@@ -1,18 +1,32 @@
-from django.conf import settings
 from django.db import models
-from django.utils import timezone
 
 
-class Post(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
-    text = models.TextField()
-    created_date = models.DateTimeField(default=timezone.now)
-    published_date = models.DateTimeField(blank=True, null=True)
+class Produit(models.Model):
+    nom = models.CharField(max_length=150)
+    description = models.TextField()
+    prix = models.DecimalField(decimal_places=2,max_digits=2)
+    stock = models.IntegerField()
+    image = models.ImageField()
+    est_actif = models.IntegerField()
 
-    def publish(self):
-        self.published_date = timezone.now()
-        self.save()
+class Client(models.Model):
+    nom = models.CharField(max_length=150)
+    prenom = models.CharField(max_length=150)
+    date_de_naissance = models.DateField()
+    email = models.EmailField(max_length=30)
+    telephone = models.CharField(max_length=15)
+    pseudo = models.CharField(max_length=15)
+    password = models.CharField(max_length=40)
+    est_actif = models.IntegerField()
 
-    def __str__(self):
-        return self.title
+class Commande(models.Model):
+    id_client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    prix = models.DecimalField(decimal_places=2,max_digits=2)
+    etat = models.CharField(max_length=150)
+    type_facturation = models.CharField(max_length=100)
+    est_actif = models.IntegerField()
+
+class CommandeProduit(models.Model):
+    id_produit = models.ForeignKey(Produit, on_delete=models.CASCADE)
+    id_commande = models.ForeignKey(Commande, on_delete=models.CASCADE)
+    nombre_produit = models.IntegerField()
